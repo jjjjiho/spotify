9f3f6ccfa7bc33a25deaacdcabe6294e883df898
jjjjiho <107088057+jjjjiho@users.noreply.github.com>
2022-12-15 15:38:39 +0900

    Update Dashboard.js

--- spotify-clone-main/client/src/Dashboard.js
+++ spotify-clone-main/client/src/Dashboard.js
@@ -7,7 +7,7 @@ import SpotifyWebApi from "spotify-web-api-node"
 import axios from "axios"
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: "8b945ef10ea24755b83ac50cede405a0",
+  clientId: "2d1db12bd50d44008015d7bb551362d1",
 })
 
 export default function Dashboard({ code }) {
